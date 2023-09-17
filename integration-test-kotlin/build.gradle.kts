df3d8919f8e05b172d72ef6e1236c824ff088353
​Andrzej Ressel <github@andrzejressel.pl>
2023-09-17 20:29:06 +0200

    Implement support for java.util.Map

--- integration-test-kotlin/build.gradle.kts
+++ integration-test-kotlin/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm")
     kotlin("kapt")
@@ -14,10 +16,23 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("net.jqwik:jqwik:1.8.0")
     testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("net.jqwik:jqwik-kotlin:1.8.0")
     kapt(project(":processor"))
     implementation(project(":serializator"))
 }
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+// https://jqwik.net/docs/current/user-guide.html#build-configuration-for-kotlin
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf(
+            "-Xjsr305=strict", // Required for strict interpretation of
+            "-Xemit-jvm-type-annotations" // Required for annotations on type variables
+        )
+        jvmTarget = "11" // 1.8 or above
+        javaParameters = true // Required to get correct parameter names in reporting
+    }
 }
