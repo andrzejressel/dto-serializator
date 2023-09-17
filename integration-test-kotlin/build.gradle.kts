b0a990c348b06b65790ef210dc59f5696c74ff3a
​Andrzej Ressel <github@andrzejressel.pl>
2023-09-17 20:31:35 +0200

    Implement support for java.util.Map

--- integration-test-kotlin/build.gradle.kts
+++ integration-test-kotlin/build.gradle.kts
@@ -32,7 +32,7 @@ tasks.withType<KotlinCompile> {
             "-Xjsr305=strict", // Required for strict interpretation of
             "-Xemit-jvm-type-annotations" // Required for annotations on type variables
         )
-        jvmTarget = "11" // 1.8 or above
+//        jvmTarget = "11" // 1.8 or above
         javaParameters = true // Required to get correct parameter names in reporting
     }
 }
